3573b654e334784df6ec27e20e32aee8f1446c70
d2ni3l <107326751+d2ni3l@users.noreply.github.com>
2023-06-04 23:40:01 +0200

    adjusted problem with the width

--- Spotify/src/App.jsx
+++ Spotify/src/App.jsx
@@ -8,13 +8,13 @@ const App = () => {
   const { activeSong } = useSelector((state) => state.player);
 
   return (
-    <div className="relative flex">
+    <main className="relative flex">
       <Sidebar />
       
       <div className="flex-1 flex flex-col bg-gradient-to-br to-[#1e1e1e] from-[#040404]">
         <Searchbar />
 
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        <div className="px-6  overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
@@ -36,7 +36,7 @@ const App = () => {
           <MusicPlayer />
         </div>
       )}
-    </div>
+    </main>
   );
 };
 
--- Spotify/src/pages/Discover.jsx
+++ Spotify/src/pages/Discover.jsx
@@ -18,18 +18,7 @@ const Discover = () => {
         <h2 className='font-bold text-3xl text-white text-left'>
           Discover {title}
         </h2>
-        <select
-          onChange={() => {}}
-          value=''
-          className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5 '>
-          {genres.map((genres) => {
-            return (
-              <option key={genres.value} value={genres.value}>
-                {genres.title}              
-              </option>
-            );
-          })}
-        </select>
+       
       </div>
       <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
         {data.tracks?.map((song, i) => {
